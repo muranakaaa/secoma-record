5001b116b16cb9d675f43fe9e2452b28e54b82a2
村中信隆 <chanayataka2324@gmail.com>
2025-01-23 08:00:09 +0900

    backendのヘルスチェック用APIを実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :shops, only: [ :index, :show ]
       get "/profile", to: "users#profile"
+      get "health_check", to: "health_check#index"
     end
   end
 end
