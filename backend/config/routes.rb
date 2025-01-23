ca6faba157c044abc065667079f2419411cbc328
村中信隆 <chanayataka2324@gmail.com>
2025-01-23 17:19:05 +0900

    devise_token_authの動作確認

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   namespace :api do
     namespace :v1 do
       resources :shops, only: [:index, :show]
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "User", at: "auth"
     end
   end
+
+  root to: redirect("/api/v1/health_check")
 end
