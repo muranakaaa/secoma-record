b7d7075c7f866fcf1fb172c57277c9dc823e067a
村中信隆 <chanayataka2324@gmail.com>
2025-01-23 20:40:00 +0900

    サインインユーザーを取得するアクションの実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
       get "/profile", to: "users#profile"
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
+
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
 
