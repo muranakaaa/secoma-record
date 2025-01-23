303174dd9aa7f0325c0310fc90b381796c74df12
村中信隆 <chanayataka2324@gmail.com>
2025-01-23 16:48:07 +0900

    devise_token_authによるUserのセットアップ

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :api do
     namespace :v1 do
-      resources :shops, only: [ :index, :show ]
+      resources :shops, only: [:index, :show]
       get "/profile", to: "users#profile"
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
