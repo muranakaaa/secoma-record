0c263d60adc8e07a52962e26daf347772bad6d24
村中信隆 <chanayataka2324@gmail.com>
2025-01-24 21:32:44 +0900

    サインインページの実装

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "*"
     resource "*",
       headers: :any,
+      expose: ['access-token', 'uid','client'],
       methods: [ :get, :post, :patch, :put, :delete, :options, :head ]
   end
 end
--- frontend/app/components/common/Header.tsx
+++ frontend/app/components/common/Header.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 const Header = () => {
   return (
     <div>
-      <header className="bg-zinc-300">Header</header>
+      <header className="bg-zinc-300">Header
+      <Link href="/sign_in">
+        <Button>Sign In</Button>
+      </Link>
+      </header>
     </div>
   );
 };
