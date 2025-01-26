18a48180c7430b57219c8f4fb93f15ec6a617fa1
村中信隆 <chanayataka2324@gmail.com>
2025-01-26 18:41:39 +0900

    shopsテーブルからopening_hoursのカラムを削除

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3,14 +3,12 @@ Shop.create!([
     name: 'セイコーマート 札幌駅前店',
     address: '北海道札幌市中央区北4条西4丁目',
     latitude: 43.0687,
-    longitude: 141.3508,
-    opening_hours: '7:00 - 23:00'
+    longitude: 141.3508
   },
   {
     name: 'セイコーマート 円山店',
     address: '北海道札幌市中央区北1条西27丁目',
     latitude: 43.0569,
-    longitude: 141.3258,
-    opening_hours: '8:00 - 22:00'
+    longitude: 141.3258
   }
 ])
