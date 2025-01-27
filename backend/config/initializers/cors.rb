230a9f8071e5646e76428adafdb0016b2451765b
村中信隆 <chanayataka2324@gmail.com>
2025-01-27 22:53:24 +0900

    CORS設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-    resource "*",
+    origins 'https://secoma-record.vercel.app/'
+
+    resource '*',
       headers: :any,
-      expose: ['access-token', 'uid','client'],
-      methods: [ :get, :post, :patch, :put, :delete, :options, :head ]
+      expose: ['access-token', 'uid', 'client'],
+      methods: [:get, :post, :patch, :put, :delete, :options, :head]
+  end
+
+
+  allow do
+    origins 'http://localhost:3001'
+    resource '*',
+      headers: :any,
+      expose: ['access-token', 'uid', 'client'],
+      methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
