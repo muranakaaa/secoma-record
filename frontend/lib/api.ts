4bc9832a94797ea43e92ed9f03f3acb3e4679af1
村中信隆 <chanayataka2324@gmail.com>
2025-02-04 21:00:05 +0900

    ページネーション関連のコードを削除

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const getShops = async (page: number, perPage: number) => {
+export const getShops = async () => {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/shops`);
     return response.data;
