a252f82955f942c9d69ba4b755341ddc1a9dc00f
村中信隆 <chanayataka2324@gmail.com>
2025-02-05 23:33:09 +0900

    router.isReady の削除

--- frontend/app/confirmation/page.tsx
+++ frontend/app/confirmation/page.tsx
@@ -1,49 +1,50 @@
-"use client"
+'use client'
+
 import axios, { AxiosError } from 'axios'
 import type { NextPage } from 'next'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import { useSnackbarState } from '../hooks/useGlobalState'
 
 const Confirmation: NextPage = () => {
+  const searchParams = useSearchParams()
   const router = useRouter()
   const [, setSnackbar] = useSnackbarState()
 
   useEffect(() => {
-    if (!router.isReady) {
-      return
-    }
+    const confirmationToken = searchParams.get('confirmation_token')
 
-    if (router.query['confirmation_token']) {
-      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/confirmations`
-
-      axios({ method: 'PATCH', url: url, data: router.query })
-        .then(() => {
-          setSnackbar({
-            message: '認証に成功しました',
-            severity: 'success',
-            pathname: '/sign_in',
-          })
-          router.push('/sign_in')
-        })
-        .catch((e: AxiosError<{ error: string }>) => {
-          console.log(e.message)
-          setSnackbar({
-            message: '不正なアクセスです',
-            severity: 'error',
-            pathname: '/',
-          })
-          router.push('/')
-        })
-    } else {
+    if (!confirmationToken) {
       setSnackbar({
         message: '不正なアクセスです',
         severity: 'error',
         pathname: '/',
       })
       router.push('/')
+      return
     }
-  }, [router, setSnackbar])
+
+    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/confirmations`
+
+    axios({ method: 'PATCH', url: url, data: { confirmation_token: confirmationToken } })
+      .then(() => {
+        setSnackbar({
+          message: '認証に成功しました',
+          severity: 'success',
+          pathname: '/sign_in',
+        })
+        router.push('/sign_in')
+      })
+      .catch((e: AxiosError<{ error: string }>) => {
+        console.log(e.message)
+        setSnackbar({
+          message: '不正なアクセスです',
+          severity: 'error',
+          pathname: '/',
+        })
+        router.push('/')
+      })
+  }, [searchParams, router, setSnackbar])
 
   return <></>
 }
