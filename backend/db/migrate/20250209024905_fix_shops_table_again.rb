1a680c3dd2490b1c0911f268296e8de2b1923634
村中信隆 <chanayataka2324@gmail.com>
2025-02-11 22:21:17 +0900

    Fix migration error: remove_reference removed

--- backend/db/migrate/20250209024905_fix_shops_table_again.rb
+++ backend/db/migrate/20250209024905_fix_shops_table_again.rb
@@ -1,7 +1,5 @@
 class FixShopsTableAgain < ActiveRecord::Migration[7.2]
   def change
-    remove_reference :shops, :area, foreign_key: true
-
     add_column :shops, :area, :string unless column_exists?(:shops, :area)
   end
 end
