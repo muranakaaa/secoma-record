1773854ae965732ab92ef8b04bf008c9e62c3939
村中信隆 <chanayataka2324@gmail.com>
2025-02-13 18:02:46 +0900

    chore: CIでDotenv::Railsを使用するように変更

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -14,7 +14,7 @@ require "action_view/railtie"
 
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load if defined?(Dotenv)
+Dotenv::Rails.load if defined?(Dotenv)
 
 module Backend
   class Application < Rails::Application
