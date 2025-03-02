5102574bad021b89ad77ed38834b0a79778abec1
村中信隆 <chanayataka2324@gmail.com>
2025-03-02 10:51:40 +0900

    test: shopsコントローラーのRSpecテストを実施

--- backend/spec/factories/shops.rb
+++ backend/spec/factories/shops.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :shop do
-    name { "セイコーマート稚内中央店" }
-    address { "北海道稚内市中央４丁目６番３号" }
-    area { "稚内・留萌" }
-    sub_area { "稚内市" }
+    name { "セイコーマート札幌中央店" }
+    address { "北海道札幌市中央４丁目６番３号" }
+    area_romaji { "sapporo" }
+    sub_area_romaji { "chuou-ku" }
+    area { "札幌" }
+    sub_area { "中央区" }
   end
 end
--- backend/spec/requests/api/v1/shops_controller_spec.rb
+++ backend/spec/requests/api/v1/shops_controller_spec.rb
@@ -1,77 +1,82 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Api::V1::ShopsController, type: :request do
-#   let!(:shop1) { create(:shop, name: 'Shop A', address: 'Hokkaido', area: 'Hokkaido', sub_area: 'Sapporo') }
-#   let!(:shop2) { create(:shop, name: 'Shop B', address: 'Hokkaido', area: 'Hokkaido', sub_area: 'Asahikawa') }
-#   let!(:shop3) { create(:shop, name: 'Shop C', address: 'Hokkaido', area: 'Tokachi', sub_area: 'Obihiro') }
-#   let!(:user) { create(:user) }
-#   let!(:visit) { create(:visit, user: user, shop: shop1) }
+RSpec.describe "Api::V1::ShopsController", type: :request do
+  let!(:shop1) { create(:shop, area_romaji: "sapporo", sub_area_romaji: "chuou-ku", name: "セイコーマート札幌中央店", address: "札幌市中央区1丁目") }
+  let!(:shop2) { create(:shop, area_romaji: "sapporo", sub_area_romaji: "chuou-ku", name: "セイコーマート大通店", address: "札幌市中央区2丁目") }
+  let!(:shop3) { create(:shop, area_romaji: "wakkanai-rumoi", sub_area_romaji: "wakkanai-shi", name: "セイコーマート稚内店", address: "稚内市1丁目") }
 
-#   describe 'GET /api/v1/shops' do
-#     context 'パラメータなしで全ショップを取得する場合' do
-#       it '100件までのショップ情報を取得できる' do
-#         get '/api/v1/shops'
+  let!(:visit1) { create(:visit, shop: shop1) }
+  let!(:visit2) { create(:visit, shop: shop2) }
 
-#         expect(response).to have_http_status(:ok)
-#         json = JSON.parse(response.body)
-#         expect(json['data'].size).to be <= 100
-#       end
-#     end
+  describe "GET /api/v1/:area/:sub_area" do
+    context "正常系" do
+      it "指定したエリア・サブエリアの店舗一覧を取得できる" do
+        get "/api/v1/sapporo/chuou-ku"
 
-#     context 'sub_area パラメータを指定する場合' do
-#       it '該当するサブエリアのショップ一覧を取得する' do
-#         get '/api/v1/shops', params: { sub_area: 'Sapporo' }
+        expect(response).to have_http_status(:success)
+        json = JSON.parse(response.body)
+        expect(json["area"]).to eq("札幌")
+        expect(json["sub_area"]).to eq("中央区")
+        expect(json["shops"].size).to eq(3)
+        expect(json["shops"].first["name"]).to eq("セイコーマート札幌中央店")
+      end
+    end
 
-#         expect(response).to have_http_status(:ok)
-#         json = JSON.parse(response.body)
-#         expect(json['shops'].size).to eq(1)
-#         expect(json['shops'].first['name']).to eq('Shop A')
-#       end
-#     end
-#   end
+    context "異常系" do
+      it "該当エリア・サブエリアがない場合は404を返す" do
+        get "/api/v1/invalid-area/invalid-sub-area"
 
-#   describe 'GET /api/v1/shops/:id' do
-#     context '指定したショップが存在する場合' do
-#       it 'ショップの詳細情報を取得できる' do
-#         get "/api/v1/shops/#{shop1.id}"
+        expect(response).to have_http_status(:not_found)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("該当エリア・サブエリアが見つかりません")
+      end
+    end
+  end
 
-#         expect(response).to have_http_status(:ok)
-#         json = JSON.parse(response.body)
-#         expect(json['name']).to eq('Shop A')
-#       end
-#     end
+  describe "GET /api/v1/:area/:sub_area/:id" do
+    context "正常系" do
+      it "指定したIDの店舗情報を取得できる" do
+        get "/api/v1/sapporo/chuou-ku/#{shop1.id}"
 
-#     context '指定したショップが存在しない場合' do
-#       it '404エラーを返す' do
-#         get '/api/v1/shops/99999'
+        expect(response).to have_http_status(:success)
+        json = JSON.parse(response.body)
+        expect(json["id"]).to eq(shop1.id)
+        expect(json["name"]).to eq("セイコーマート札幌中央店")
+        expect(json["address"]).to eq("札幌市中央区1丁目")
+      end
+    end
 
-#         expect(response).to have_http_status(:not_found)
-#         json = JSON.parse(response.body)
-#         expect(json['error']).to eq('Shop not found')
-#       end
-#     end
-#   end
+    context "異常系" do
+      it "存在しないIDを指定すると404を返す" do
+        get "/api/v1/sapporo/chuou-ku/9999"
 
-#   describe 'GET /api/v1/shops/search_shops' do
-#     context '検索キーワードがある場合' do
-#       it '該当するショップを取得する' do
-#         get '/api/v1/shops/search_shops', params: { query: 'Shop A' }
+        expect(response).to have_http_status(:not_found)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("該当する店舗が見つかりません")
+      end
+    end
+  end
 
-#         expect(response).to have_http_status(:ok)
-#         json = JSON.parse(response.body)
-#         expect(json['shops'].size).to eq(1)
-#         expect(json['shops'].first['name']).to eq('Shop A')
-#       end
-#     end
+  describe "GET /api/v1/shops/search_shops" do
+    context "正常系" do
+      it "店舗名で部分一致検索ができる" do
+        get "/api/v1/shops/search_shops", params: { query: "札幌" }
 
-#     context '検索キーワードが空の場合' do
-#       it '400エラーを返す' do
-#         get '/api/v1/shops/search_shops', params: { query: '' }
+        expect(response).to have_http_status(:success)
+        json = JSON.parse(response.body)
+        expect(json["shops"].size).to eq(1)
+        expect(json["shops"].first["name"]).to include("札幌")
+      end
+    end
 
-#         expect(response).to have_http_status(:bad_request)
-#         json = JSON.parse(response.body)
-#         expect(json['error']).to eq('検索キーワードが必要です')
-#       end
-#     end
-#   end
-# end
+    context "異常系" do
+      it "検索クエリがない場合は404を返す" do
+        get "/api/v1/shops/search_shops"
+
+        expect(response).to have_http_status(:bad_request)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("検索クエリを指定してください")
+      end
+    end
+  end
+end
