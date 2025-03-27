2a9af493e165f53152cfa82559e516aeb28970c5
村中信隆 <chanayataka2324@gmail.com>
2025-03-27 10:10:14 +0900

    fix: awaitを追加

--- frontend/lib/fetchAreasWithToken.ts
+++ frontend/lib/fetchAreasWithToken.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
 export const fetchAreasWithToken = async () => {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const token = cookieStore.get("access-token")?.value || "";
   const client = cookieStore.get("client")?.value || "";
   const uid = cookieStore.get("uid")?.value || "";
